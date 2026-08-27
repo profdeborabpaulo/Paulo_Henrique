781f2ea5561154e41288a02a7873e48f85924929
Paulo Henrique Juvencio <pauloh081033@gmail.com>
2026-08-27 10:50:26 -0300

    versão 3
    versão atualizada do projeto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,49 @@
-import{View, Text, TextInput, StyleSheet} from 'react-native';
+// importando os componentes do react-native
+import { View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import { useState } from 'react';
 
-export default function HomeScreen( ) {
+export default function HomeScreen( ){
+  const[tarefa, setTarefa]= useState('');
+  const[tarefas, setTarefas]= useState([
+    "Estudar React-Native",
+    "Aprender useState",
+    "Criar a primeira tela",
+  ]);
+  function adicionarTarefa( ){
+    if(!tarefa.trim())//impede tarefas em branco
+      return;
+      setTarefas([...tarefas, tarefa]);
+      setTarefa('');
+  }
+  // Próxima etapa: 
   return(
-    <View>
-      <Text>Gerenciador de Tarefas</Text>
-      <TextInput
-      placeholder="Digite uma tarefa:"
+    <View style={styles.container }>
+      
+      <Text style={styles.title}> Gerenciador de Tarefas</Text>
+
+      <TextInput style={styles.input}
+      placeholder='Digite uma tarefa'
       />
+
     </View>
   );
-  
 }
+
+//Configurando o estilo do index.tsx
+const styles=StyleSheet.create({
+  container:{
+    flex: 1,
+    padding: 20,
+  },
+  title:{
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 12,
+  },
+});
